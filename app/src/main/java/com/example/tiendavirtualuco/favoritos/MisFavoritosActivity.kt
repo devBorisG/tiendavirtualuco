9bd5538f7301c8330c8c262f0a4e8dad9a4586a6
Juan Cardona <felipe7u7432@gmail.com>
2024-08-25 01:23:32 -0500

    :lipstick: NEW FEATURE: Agregar vista de mis favoritos

---
+++ app/src/main/java/com/example/tiendavirtualuco/favoritos/MisFavoritosActivity.kt
@@ -0,0 +1,13 @@
+package com.example.tiendavirtualuco.favoritos
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tiendavirtualuco.R
+
+class MisFavoritosActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mis_favoritos)
+    }
+}
