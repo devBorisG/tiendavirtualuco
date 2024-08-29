844510f9a3f4e6681a3e599316ab86d209d8b8c2
Juan Cardona <felipe7u7432@gmail.com>
2024-08-29 16:32:23 -0500

    :sparkles: NEW FEATURE: Agregar cuerpo de la vista detalle de producto
    Issue: #4

--- app/src/main/java/com/example/tiendavirtualuco/detalleproducto/ModeloDetalleProducto.kt
+++ app/src/main/java/com/example/tiendavirtualuco/detalleproducto/ModeloDetalleProducto.kt
@@ -3,6 +3,7 @@ package com.example.tiendavirtualuco.detalleproducto
 data class ModeloDetalleProducto (
     val nombre: String,
     val precio: String,
+    val precioOriginal: String,
     val descripcion: String,
     val imagen: String,
     val nombreTienda: String
