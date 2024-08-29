785e1574e5818c7b06662e0a20f2bfcb960bebbb
Juan Cardona <felipe7u7432@gmail.com>
2024-08-29 13:23:23 -0500

    :sparkles: NEW FEATURE: Agregar modelo de detalle del producto
    Issue: #4

---
+++ app/src/main/java/com/example/tiendavirtualuco/detalleproducto/ModeloDetalleProducto.kt
@@ -0,0 +1,9 @@
+package com.example.tiendavirtualuco.detalleproducto
+
+data class ModeloDetalleProducto (
+    val nombre: String,
+    val precio: String,
+    val descripcion: String,
+    val imagen: String,
+    val nombreTienda: String
+)
