644c79b6401d19963f4f270c131771d4b6ed0de7
David Andrés Valencia García <david.andres.2801@gmail.com>
2024-09-02 23:18:28 -0500

    :sparkles: NEW FEATURE: Se agrega la funcionalidad de carga de imagenes en el RecyclerView para los productos

--- app/src/main/java/com/example/tiendavirtualuco/homepageproductos/adapter/VistaProducto.kt
+++ app/src/main/java/com/example/tiendavirtualuco/homepageproductos/adapter/VistaProducto.kt
@@ -3,6 +3,7 @@ package com.example.tiendavirtualuco.homepageproductos.adapter
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tiendavirtualuco.homepageproductos.ModeloProducto
+import com.bumptech.glide.Glide
 
 class VistaProducto(view : View) : RecyclerView.ViewHolder(view) {
     val imagenProducto = view.findViewById<android.widget.ImageView>(com.example.tiendavirtualuco.R.id.imageViewProducto)
@@ -12,6 +13,10 @@ class VistaProducto(view : View) : RecyclerView.ViewHolder(view) {
     fun render(modeloProducto: ModeloProducto) {
         nombreProducto.text = modeloProducto.nombreProducto
         precioProducto.text = modeloProducto.precioProducto
-        // imagenProducto.setImageResource(modeloProducto.imagenProducto)
+        Glide.with(itemView.context)
+            .load(modeloProducto.imagenProducto)
+            .override(150, 150)
+            .centerCrop()
+            .into(imagenProducto)
     }
 }
