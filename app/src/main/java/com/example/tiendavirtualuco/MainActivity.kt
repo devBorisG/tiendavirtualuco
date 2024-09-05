e2de71c84fd0fbbfd9979b53edd38dc2cd26f561
jerofranco8 <100442372+jerofranco8@users.noreply.github.com>
2024-09-04 22:46:50 -0500

    Comienzo

--- app/src/main/java/com/example/tiendavirtualuco/MainActivity.kt
+++ app/src/main/java/com/example/tiendavirtualuco/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.trazabilidad_principal)
+        setContentView(R.layout.detalle_producto)
     }
 }
--- app/src/main/java/com/example/tiendavirtualuco/homepageproductos/adapter/VistaProducto.kt
+++ app/src/main/java/com/example/tiendavirtualuco/homepageproductos/adapter/VistaProducto.kt
@@ -3,7 +3,7 @@ package com.example.tiendavirtualuco.homepageproductos.adapter
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tiendavirtualuco.homepageproductos.ModeloProducto
-import com.bumptech.glide.Glide
+//import com.bumptech.glide.Glide
 
 class VistaProducto(view : View) : RecyclerView.ViewHolder(view) {
     val imagenProducto = view.findViewById<android.widget.ImageView>(com.example.tiendavirtualuco.R.id.imageViewProducto)
@@ -13,10 +13,12 @@ class VistaProducto(view : View) : RecyclerView.ViewHolder(view) {
     fun render(modeloProducto: ModeloProducto) {
         nombreProducto.text = modeloProducto.nombreProducto
         precioProducto.text = modeloProducto.precioProducto
-        Glide.with(itemView.context)
+        /*Glide.with(itemView.context)
             .load(modeloProducto.imagenProducto)
             .override(150, 150)
             .centerCrop()
             .into(imagenProducto)
+
+         */
     }
 }
