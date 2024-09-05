3b903540371d4195d45155f1b6c6d8201b771f18
Juan Cardona <felipe7u7432@gmail.com>
2024-09-05 17:04:30 -0500

    :sparkles: NEW FEATURE: Agregar imagen a la vista
    Issue: #18

--- app/src/main/java/com/example/tiendavirtualuco/detalleproducto/DetalleProductoActivity.kt
+++ app/src/main/java/com/example/tiendavirtualuco/detalleproducto/DetalleProductoActivity.kt
@@ -1,13 +1,24 @@
 package com.example.tiendavirtualuco.detalleproducto
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.tiendavirtualuco.R
 
 class DetalleProductoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.detalle_producto)
+        setImage()
+    }
+
+    fun setImage(){
+        val imgProducto = findViewById<ImageView>(R.id.imgProducto)
+        Glide.with(this)
+            .load("https://http2.mlstatic.com/D_NQ_NP_2X_629321-MLA52579227077_112022-F.webp")
+            .override(300,140)
+            .centerCrop()
+            .into(imgProducto)
     }
 }
