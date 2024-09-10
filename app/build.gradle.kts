2188fb73d037e8879df1ab5f8affbe24643136c8
jerofranco8 <100442372+jerofranco8@users.noreply.github.com>
2024-09-09 20:22:00 -0500

    Agregar Dependecias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.glide.v4142)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
